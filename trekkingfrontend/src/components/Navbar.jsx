d1f6e91bcacae63f4a5311a135f7e0f7cf755444
allan <allan.gtm92@gmail.com>
2025-06-18 07:32:37 +0545

    enhanced navbar to have bold for current page

--- trekkingfrontend/src/components/Navbar.jsx
+++ trekkingfrontend/src/components/Navbar.jsx
@@ -1,42 +1,62 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-[#F6FFFF] text-black p-4 shadow-sm">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <Link to="/" className="text-xl font-bold">
-          <img src="/logo.png" alt="Trekking Website Logo" className="h-12" />
-        </Link>
+        <NavLink to="/" className="text-xl font-bold">
+          <img src="/logo.png" alt="[Company Name] Logo" className="h-8" />
+        </NavLink>
 
         {/* Center Links */}
         <div className="flex space-x-8">
-          <Link to="/" className="hover:text-blue-500">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "font-bold hover:text-blue-500" : "hover:text-blue-500"
+            }
+          >
             Home
-          </Link>
-          <Link to="/packages" className="hover:text-blue-500">
+          </NavLink>
+          <NavLink
+            to="/packages"
+            className={({ isActive }) =>
+              isActive ? "font-bold hover:text-blue-500" : "hover:text-blue-500"
+            }
+          >
             Packages
-          </Link>
-          <Link to="/about" className="hover:text-blue-500">
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "font-bold hover:text-blue-500" : "hover:text-blue-500"
+            }
+          >
             About Us
-          </Link>
-          <Link to="/contact" className="hover:text-blue-500">
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? "font-bold hover:text-blue-500" : "hover:text-blue-500"
+            }
+          >
             Contact Us
-          </Link>
+          </NavLink>
         </div>
 
         {/* Right Actions */}
         <div className="flex items-center space-x-4">
-          <Link to="/search" aria-label="Search">
+          <NavLink to="/search" aria-label="Search">
             <MagnifyingGlassIcon className="h-6 w-6 hover:text-blue-500" />
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/login"
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           >
             Login
-          </Link>
+          </NavLink>
         </div>
       </div>
     </nav>
