e8fbac112175b27ca51310cf9eb4c94e5c61c0fe
allan <allan.gtm92@gmail.com>
2025-07-04 20:11:07 +0545

    added scrollToTop to easy navigation

--- trekkingfrontend/src/App.jsx
+++ trekkingfrontend/src/App.jsx
@@ -1,17 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import About from "./components/About";
-import ErrorBoundary from "./components/ErrorBoundary";
-import Footer from "./components/Footer";
-import Homepage from "./components/Homepage";
-import Login from "./components/Login";
-import Navbar from "./components/Navbar";
-import PackageDetails from "./components/PackageDetails";
-import PackageList from "./components/PackageList";
-import SignUp from "./components/SignUp";
+import ScrollToTop from "./components/ScrollToTop";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+import ErrorBoundary from "./pages/ErrorBoundary";
+import Footer from "./pages/Footer";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Navbar from "./pages/Navbar";
+import PackageDetails from "./pages/PackageDetails";
+import PackageList from "./pages/PackageList";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex-grow pt-[80px] ">
@@ -23,7 +26,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<div>Contact Us</div>} />
+              <Route path="/contact" element={<ContactUs />} />
               <Route path="/search" element={<div>Search</div>} />
             </Routes>
           </ErrorBoundary>
