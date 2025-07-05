20dafe40d11d385dd6a039b7616b0370af9541a3
allan <allan.gtm92@gmail.com>
2025-07-05 12:28:22 +0545

    minor bug fix

--- trekkingfrontend/vite.config.js
+++ trekkingfrontend/vite.config.js
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/trekking-website",
+  base: "/trekkingwebsite",
 });
