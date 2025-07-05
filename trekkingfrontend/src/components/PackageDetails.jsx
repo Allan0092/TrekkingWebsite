b9c2302c9ab24fe50056ea9ad73c484407687b66
allan <allan.gtm92@gmail.com>
2025-07-05 17:10:17 +0545

    minor bug fix

--- trekkingfrontend/src/components/PackageDetails.jsx
+++ trekkingfrontend/src/components/PackageDetails.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import PackageImageUpload from "../pages/PackageImageUpload";
+import PackageImageUpload from "../components/PackageImageUpload";
 
 const PackageDetails = () => {
   const { id } = useParams();
