657be751e2feca8baf6df0706415ff09c5da16eb
allan <allan.gtm92@gmail.com>
2025-07-10 21:22:47 +0545

    login and registration success

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
+    'rest_framework.authtoken',  
     'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
